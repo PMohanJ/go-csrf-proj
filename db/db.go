671b976d9b5bc3b0f1012f11601c59efd274c3d5
pmohanj <pmohan6712@gmail.com>
2022-10-18 16:28:14 +0530

    Implement file system database

--- db/db.go
+++ db/db.go
@@ -2,25 +2,68 @@ package db
 
 import (
 	"errors"
+	"log"
 
 	"github.com/pmohanj/golang-csrf-project/db/models"
+	"github.com/pmohanj/golang-csrf-project/randomstrings"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var users = map[string]models.User{}
 
-func InitDB() {
+var refreshTokens map[string]string
 
+func InitDB() {
+	refreshTokens = make(map[string]string)
 }
 
 func StoreUser(username, password, role string) (uuid string, err error) {
+	uuid, err = randomstrings.GenerateRandomString(32)
+	if err != nil {
+		return "", err
+	}
 
+	// Make sure out newly created uuid is unique
+	u := models.User{}
+	for u != users[uuid] {
+		uuid, err = randomstrings.GenerateRandomString(32)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	// Generate the bcrypt password hash
+	passwordHash, hashErr := generateBcryptHash(password)
+	if hashErr != nil {
+		return
+	}
+
+	users[uuid] = models.User{
+		Username:     username,
+		PasswordHash: passwordHash,
+		Role:         role}
+
+	return uuid, err
 }
 
-func DeleteUser() {
-
+func DeleteUser(uuid string) {
+	delete(users, uuid)
 }
 
-func FetchUserById() {
+func FetchUserById(uuid string) (models.User, error) {
+
+	user := users[uuid]
+	emptyUser := models.User{}
+
+	// Go doesn't have any methods for map that raises any error
+	// if that key isn't present in map
+	if emptyUser != user {
+
+		// found the user return it
+		return user, nil
+	} else {
+		return emptyUser, errors.New("User not found")
+	}
 
 }
 
@@ -33,26 +76,48 @@ func FetchUserByUsername(username string) (models.User, string, error) {
 	return models.User{}, "", errors.New("User not found that matches given username")
 }
 
-func StoreRefreshToken() {
+func StoreRefreshToken() (jti string, err error) {
+	jti, err = randomstrings.GenerateRandomString(32)
+	if err != nil {
+		return jti, err
+	}
 
+	// check to make sure our jti is unique
+	for refreshTokens[jti] != "" {
+		jti, err = randomstrings.GenerateRandomString(32)
+		if err != nil {
+			return jti, err
+		}
+	}
+
+	refreshTokens[jti] = "valid"
+
+	return jti, err
 }
 
-func DeleteRefreshToken() {
-
+func DeleteRefreshToken(jti string) {
+	delete(refreshTokens, jti)
 }
 
-func CheckRefreshToken() bool {
-
+func CheckRefreshToken(jti string) bool {
+	return refreshTokens[jti] != ""
 }
 
-func LogUserInt() (models.User, string, error) {
+func LogUserIn(username, password string) (models.User, string, error) {
+	user, uuid, userErr := FetchUserByUsername(username)
+	log.Println(user, uuid, userErr)
+	if userErr != nil {
+		return models.User{}, "", userErr
+	}
 
+	return user, uuid, checkPassordAgainstHash(user.PasswordHash, password)
 }
 
-func generateBcryptHash() (string, error) {
-
+func generateBcryptHash(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(hash[:]), err
 }
 
-func checkPassordAgainstHash() error {
-
+func checkPassordAgainstHash(hashedPass, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPass), []byte(password))
 }
